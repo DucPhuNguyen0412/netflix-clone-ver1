80b1d355ea31ba8d88a214f8325e5eab3bf46023
Phu Nguyen <phutom0412panda@gmail.com>
2023-12-27 18:57:35 -0500

    Solve nginx not found staticfiles

--- netflix_site/settings.py
+++ netflix_site/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-5+n$*!xo2dz68e3ek&_8-x(q35&4@k87vgnwngm7ww05%g3-w@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
@@ -124,9 +124,9 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'assets'), 
+# ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
