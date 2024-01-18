675d0b9c2c547dd27cdcd6dc79b20329184674ac
Phu Nguyen <phutom0412panda@gmail.com>
2024-01-18 17:48:44 -0500

    update settings.py to fix S3 bucket

--- netflix_site/settings.py
+++ netflix_site/settings.py
@@ -138,6 +138,7 @@ if not RUNNING_TESTS:
     AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_BUCKET_NAME')
     AWS_S3_REGION_NAME = 'us-east-1'  # Adjust as needed
     STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+    DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
     MEDIA_URL = f'https://{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com/media/'
 else:
     # Static and media files settings for local storage during tests
